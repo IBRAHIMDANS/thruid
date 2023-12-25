2c38ffb20ec3b09432e82b36bdc74e73064e1119
ibrahimdans <ibrahima.dansoko@outlook.com>
2023-12-25 13:18:54 +0100

    chore: step green

--- stc/tests/posting-message.spec.ts
+++ stc/tests/posting-message.spec.ts
@@ -16,15 +16,27 @@ describe('Feature: Posting a message', () => {
         })
     })
 })
+let message: { id: string; text: string; author: string; publishedAt: Date; } = {
+    id: 'message-id',
+    text: 'Hello, world!',
+    author: 'A',
+    publishedAt: new Date("2023-12-25T12:00:00.000Z")
+}
+let now: Date;
 
-function givenNowIs(date: Date) {
-    // ...
+function givenNowIs(_now: Date) {
+    now = _now
 }
 
 function whenUserPostsAMessage(postMessageCommand: { id: string; text: string; author: string; }) {
-    // ...
+    message = {
+        id: postMessageCommand.id,
+        text: postMessageCommand.text,
+        author: postMessageCommand.author,
+        publishedAt: now
+    }
 }
 
 function thenPostedMessageShouldBe(expectedMessage: { id: string; text: string; author: string; publishedAt: Date; }) {
-    return expect(true).toBe(false)
+    return expect(expectedMessage).toEqual(message)
 }
