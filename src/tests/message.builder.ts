527ca1938d2b1c7826f81c1487ac07894c37dc67
ibrahimdans <ibrahima.dansoko@outlook.com>
2023-12-27 14:09:09 +0100

    chore: enhance builder message

--- src/tests/message.builder.ts
+++ src/tests/message.builder.ts
@@ -1,11 +1,46 @@
 import {Message} from "../message";
 
-export const messageBuilder = () => ({
-    buildMessage: (message: Partial<Message>): Message => ({
-        id: message.id || 'message-id',
-        text: message.text || 'Hello World',
-        author: message.author || 'A',
-        publishedAt: message.publishedAt || new Date('2023-12-26T16:39:00Z'),
-        publicationTime: message.publicationTime || '1 minute ago'
-    })
-})
+export const messageBuilder = (
+    {
+        id = 'message-id',
+        author = 'A',
+        publishedAt = new Date('2023-12-26T16:39:00Z'),
+        text = 'Hello World',
+        publicationTime = 'less than a minute ago'
+    } = {} as Message
+) => {
+    const message = {
+        id,
+        author,
+        text,
+        publishedAt,
+        publicationTime
+    };
+
+    return {
+        withId(id: string) {
+            return messageBuilder({...message, id})
+        },
+        withText(text: string) {
+            return messageBuilder({...message, text})
+        },
+        authorBy(author: string) {
+            return messageBuilder({...message, author})
+        },
+        publishedAt(publishedAt: Date) {
+            return messageBuilder({...message, publishedAt})
+        },
+        publicationTime(publicationTime: string) {
+            return messageBuilder({...message, publicationTime})
+        },
+        build(): Message {
+            return {
+                id: message.id,
+                text: message.text,
+                author: message.author,
+                publishedAt: message.publishedAt
+
+            }
+        }
+    }
+}
