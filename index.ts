e6c0de53485255510503dc8b60ff6dff15df02a1
ibrahimdans <ibrahima.dansoko@outlook.com>
2023-12-27 11:40:25 +0100

    feat: adding logic show message and update logic id

--- index.ts
+++ index.ts
@@ -2,6 +2,8 @@
 import {Command} from 'commander';
 import {PostMessageCommand, PostMessageUseCase} from "./src/post-message.usecase";
 import {FileSystemMessageRepository} from "./src/message.fs.repository";
+import * as crypto from "crypto";
+import {ViewTimelineUsecase} from "./src/view-timeline.usecase";
 
 class DateProvider implements DateProvider {
     getNow(): Date {
@@ -13,6 +15,7 @@ const program = new Command();
 const messageRepository = new FileSystemMessageRepository();
 const dateProvider = new DateProvider();
 const postMessageUseCase = new PostMessageUseCase(messageRepository, dateProvider)
+const viewTimelineUseCase = new ViewTimelineUsecase(messageRepository, dateProvider)
 program
     .version('1.0.0')
     .description('Thruid CLI')
@@ -22,7 +25,7 @@ program
             .argument('<message>', 'the message to post')
             .action(async (user, message) => {
                 const postMessageCommand: PostMessageCommand = {
-                    id: 'message-id',
+                    id: crypto.randomBytes(16).toString('hex'),
                     text: message,
                     author: user
                 }
@@ -37,6 +40,21 @@ program
                 }
             })
             .alias('p')
+    )
+    .addCommand(
+        new Command('view')
+            .argument('<user>', 'the current user')
+            .action(async (user) => {
+                try {
+                    const timeline = await viewTimelineUseCase.handle({user})
+                    console.table(timeline)
+                    process.exit(0)
+                } catch (e) {
+                    console.log(e.message, "<---- error")
+                    process.exit(1)
+                }
+            })
+            .alias('v')
     )
 ;
 
